77c215c8ef0c38972b1af3d810e94434758ccc7a
baiwoplus <2437229278@qq.com>
2022-04-17 14:02:40 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,8 @@
+# Script Name       : fileinfo.py
+# Author                : Not sure where I got this from
+# Created               : 28th November 2011
+# Last Modified     :
+# Version               : 1.0
+# Modifications     :
+
+# Description           : Show file information for a given file
